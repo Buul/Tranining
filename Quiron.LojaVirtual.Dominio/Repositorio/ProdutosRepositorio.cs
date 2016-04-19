dc2558d7af1b651152b2917ee49b98b43ca379b7
PAULO OTAVIO FIRMINO SILVA <pof_cc@hotmail.com>
2016-04-19 20:52:05 -0300

    Entity

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly  EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Produto> Produtos => _context.Produtos;
+    }
+}
